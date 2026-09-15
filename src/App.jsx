89ac75f6bc41ced10166308efbfaaaf70e07b32a
OmarQasoma <o.qasoma@gmail.com>
2026-09-15 12:51:23 +0300

    Add dog details page-page CSS and API fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import Home from './pages/Home'
+import { useEffect, useState } from "react";
+import Home from "./pages/Home";
 
 function App() {
-  return(
-    <Home />
-  )
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.jsonbin.io/v3/b/68ccf99ad0ea881f4082d5b8")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogs(data.record);
+      });
+  }, []);
+
+  return <Home />;
 }
-export default App
+
+export default App;
