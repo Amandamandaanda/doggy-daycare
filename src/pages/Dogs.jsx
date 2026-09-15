dd8eb95dec81c2af073ac2ab20c61aeb13fb0a0f
Pinar <pinarbildirici87@gmail.com>
2026-09-15 12:38:57 +0200

    Add DogCard and fix small bugs

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -5,9 +5,19 @@ function Dogs({ dogs }) {
   return (
     <main className="dogs-page">
       <h1>Våra hundar</h1>
+
       <p className="dogs-page__intro">
         Här hittar du alla hundar på Doggy Daycare.
       </p>
+
+      <div className="dogs-page__grid">
+        {dogs.map((dog) => (
+          <DogCard
+            key={dog.chipNumber}
+            dog={dog}
+          />
+        ))}
+      </div>
     </main>
   );
 }
